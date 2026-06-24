f053d31da0799be4d2fe9cfe51dbd7481e63fe65
nirvale <nirvale@gmail.com>
2026-06-23 18:29:58 -0600

    tercera ronda

--- resources/views/components/intranet/fases/grupos-tabs.blade.php
+++ resources/views/components/intranet/fases/grupos-tabs.blade.php
@@ -10,16 +10,16 @@
 
     <input id="tabFaseDos" type="radio" name="my_tabs_2" class="tab" aria-label="{{__('Second Round')}}"  {{ $roundIdActive == 2 ? 'checked' : '' }}/>
     <div class="tab-content border-base-300 bg-base-100 p-0 lg:p-2">
-      <livewire:group-fase-table :round=2 :stage=1 :suscDisabled=false />
+      <livewire:group-fase-table :round=2 :stage=1 :suscDisabled=true />
     </div>
 
 
     <input id="tabFaseTres" type="radio" name="my_tabs_2" class="tab" aria-label="{{__('Third Round')}}"  {{ $roundIdActive == 3 ? 'checked' : '' }}/>
-    <div class="tab-content border-base-300 bg-base-100 p-0 lg:p-20">
-      <div class="flex w-full items-center justify-center">
-         <span class="animate-ping text-xl text-primary  font-bold">{{__('Coming Soon')}}</span>
+
+      <div class="tab-content border-base-300 bg-base-100 p-0 lg:p-2">
+         <livewire:group-fase-table :round=3 :stage=1 :suscDisabled=false />
       </div>
-    </div>
+
   </div>
 
 </div>
